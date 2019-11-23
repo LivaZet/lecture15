e5876efaf6f539222b7515b4a8f56a783940509d
LivaZet <liva.zankovska@gmail.com>
2019-11-23 13:04:52 +0200

    progress before lunch

--- src/com/accenture/lecture15/Weather.java
+++ src/com/accenture/lecture15/Weather.java
@@ -1,5 +1,28 @@
 package com.accenture.lecture15;
 
-public class Weather {
+import java.util.Arrays;
 
+public class Weather {
+	final String country;
+	private int[] daysWeather;
+	
+	public String getCountry() {
+		return this.country;
+	}
+	public int[] getDaysWeather() {
+		return daysWeather;
+	}
+	public void setDaysWeather(int[] daysWeather) {
+		this.daysWeather = daysWeather;
+	}
+	public Weather(String country, int[] daysWeather) {
+		this.country = country;
+		this.daysWeather = daysWeather;
+	}
+	public String toString(){
+		return "Weather forecast for " + country + " next days " + daysWeather.toString();
+	}
+	
 }
+
+
